e7846e3db4f51623c3ffcd3f4587a518677e87c0
anhyeryeon2 <100520490+anhyeryeon2@users.noreply.github.com>
2022-05-15 23:39:39 +0900

    Add files via upload

---
+++ postfix-evaluation.py
@@ -0,0 +1,47 @@
+class Stack:
+    def __init__(self):  # stack 초기화
+        self.data = []
+
+    def size(self):
+        return len(self.data)
+
+    def is_empty(self):
+        return self.size() == 0
+
+    def push(self,elem):
+        self.data.append(elem)
+
+    def pop(self):
+        if self.is_empty():
+            return "error"
+        return self.data.pop()
+
+    def top(self):
+        if self.is_empty():
+            return "error"
+        return self.data[-1]
+
+def postfixeval(expr):
+    s = Stack()
+    for token in expr:
+        if token in "+-*/":
+            op2 = s.pop()
+            op1 = s.pop()
+            if token =='+':
+                s.push(op1+op2)
+            elif token == '-':
+                s.push(op1 - op2)
+            elif token == '*':
+                s.push(op1 * op2)
+            elif token == '/':
+                s.push(op1 / op2)
+            elif token == '%':
+                s.push(op1 % op2)
+        else:
+            s.push(int(token))
+    return s.pop()
+
+expr = input()
+print(postfixeval(expr))
+
+	
